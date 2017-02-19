0db2926f5a0f6420774ff2c28c72cea9960e93fa
matthework <matthework@hotmail.com>
2017-02-20 05:26:39 +1300

    update

--- privacy/mycode.py
+++ privacy/mycode.py
@@ -1,7 +1,12 @@
-import io, nltk, ntpath, PyPDF2, docx
+import io, re, nltk, ntpath, PyPDF2, docx
 from nltk import word_tokenize
 from urllib import request, error
 from bs4 import BeautifulSoup
+from nltk.parse.stanford import StanfordDependencyParser
+import time
+# import threading
+# import multiprocessing
+from multiprocessing import Pool
 
 nltk.data.path.append('./nltk_data/')
 
@@ -105,24 +110,79 @@ def doDoc(kws, path):
 
 
 def processRaw(kws, raw):
-	if raw:
-		tokens = word_tokenize(raw)
-		kwords = []
-		for k in kws:
-			kwords.append(k.name.lower())
+	# # kws = ["prove", "identity", "approval", "gain", "allowed", "password", "passwords", "username", "authentication", "access", "code" , "secret"]
+	# kws = ["cost", "cyber-crime", "estimated", "$", "445", "billion", "266", "cyber-espionage", "stealing", "individuals", "information", "believed", "800", "million", "people", "financial-losses", "cyber-theft", "cause", "150,000", "europeans", "lose", "jobs", "report", "conducted", "internet-security", "company-mcafee", "damages", "trade", "nations", "competitiveness", "innovation", "growth", "slows", "pace", "mcafee", "calling", "governments", "begin", "effort", "collect", "publish", "data", "help", "countries", "companies", "make", "choices", "risk" , "policy"]
+	# if raw:
+	# 	tokens = word_tokenize(raw)
+	# 	print(len(tokens))
+	# 	kwords = []
+	# 	for k in kws:
+	# 		# kwords.append(k.name.lower())
+	# 		kwords.append(k.lower())
 
-		text = []
-		for s in tokens:
-			if(s.lower() in kwords):
-				tts = "<input onclick='responsiveVoice.speak(\\\"" + s + "\\\");' type='button' value='🔊 xxxxx' />"
-				text.append(tts) 
-			else:
-				text.append(s)
-		result = ' '.join(text)
-	else:
-		result = 'The url is invalid or the file is not found!'
+	# 	text = []
+	# 	for s in tokens:
+	# 		if(s.lower() in kwords):
+	# 			tts = "<input onclick='responsiveVoice.speak(\\\"" + s + "\\\");' type='button' value='🔊 xxxxx' />"
+	# 			text.append(tts) 
+	# 		else:
+	# 			text.append(s)
+	# 	result = ' '.join(text)
+	# else:
+	# 	result = 'The url is invalid or the file is not found!'
+
+	
+	# out = {}
+	# sen1 = """The cost of cyber-crime for the global economy has been estimated at $445 billion ( 266 billion) annually."""
+	# sen2 = """Cyber-espionage and stealing individuals' personal information is believed to have affected more than 800 million people during 2013."""
+
+
+	# process_a = multiprocessing.Process(target=getDependency(dep_parser,sen1))
+	# process_b = multiprocessing.Process(target=getDependency(dep_parser,sen2))
+
+	# process_b.start()
+	# process_a.start()
+
+
+	# process_a.join
+	# process_b.join
+
+	# a_thread = threading.Thread(target=getDependency(dep_parser,sen1))
+	# a_thread.start()
+
+	# b_thread = threading.Thread(target=getDependency(dep_parser,sen2))
+	# b_thread.start()
+
+	# a_thread.join()
+	# b_thread.join()
+
+	# uds1 = [list(parse.triples()) for parse in dep_parser.raw_parse(sen1)]
+	# uds2 = [list(parse.triples()) for parse in dep_parser.raw_parse(sen2)]
+
+	senlist = raw.split('. ')
+	# for index, s in enumerate(senlist):
+	# 	print('('+ str(index) + ')' +s)
+
+	start = time.time()
+
+	results = Pool().map(getDependency, senlist)
+
+	print(results)
+
+	# uds = [list(parse.triples()) for parse in dep_parser.raw_parse(raw)]
+	end = time.time()
+	print('Processing time: ' + str(end-start))
+	# print(uds)
+	tokens = word_tokenize(raw)
+	print(len(tokens))
+	result = 'done!'
 	return result + '<br><br><br><br><br><br>'
 
+
+def getDependency(sen):
+	dep_parser=StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+	uds = [list(parse.triples()) for parse in dep_parser.raw_parse(sen)]
+	return str(uds)
 
 def sortKey(kws, cats):
 	result = {}
@@ -134,6 +194,21 @@ def sortKey(kws, cats):
 
 	return result
 
+
+def testRaw(raw, stops):
+
+	# tokens = word_tokenize(raw)
+	ww = re.findall(r'\b([a-zA-Z]+)\b', raw)
+	words = [w.lower() for w in ww]
+	print(words)
+
+	list1 = words
+	for s in stops:
+		while s in list1 : list1.remove(s)
+	
+	# remove digits and punctuation
+
+	print(list1)
 
 
 
--- privacy/urls.py
+++ privacy/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^web/$', views.web, name='web'),
     url(r'^doc/$', views.doc, name='doc'),
     url(r'^keyword/$', views.keyword, name='keyword'),
+    url(r'^kw/$', views.kw, name='kw'),
     url(r'^about/$', views.about, name='about'),
 ]
